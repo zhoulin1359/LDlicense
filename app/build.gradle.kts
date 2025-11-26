46c3250ef04a4c8b58bcbb6f7bd2ab988d88e5dd
jeemu <jeemu>
2025-11-26 16:08:50 +0800

    init

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 29
         targetSdk = 36
         versionCode = 1
-        versionName = "1.0.1"
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
